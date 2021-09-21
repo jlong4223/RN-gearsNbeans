f15359b6304f61621ee4069ab347c2383b1c3f1e
Jared Long <jlong4223@gmail.com>
2021-09-21 15:57:02 -0500

    feat: adds map and text to info/home tab

--- src/components/screens/tabroutes/InfoRoute.js
+++ src/components/screens/tabroutes/InfoRoute.js
@@ -1,10 +1,50 @@
 import React from 'react';
-import { Text, View } from 'native-base';
+import MapView from 'react-native-maps';
+import { VStack, Text } from 'native-base';
+import { ScrollView } from 'react-native';
+import GBLogo from '../../shared/GBLogo';
 
 export default function InfoRoute() {
+  const styles = getStyles();
+
   return (
-    <View>
-      <Text>Info Route</Text>
-    </View>
+    <ScrollView>
+      <VStack style={styles.container}>
+        <GBLogo />
+        <Text style={styles.text}>
+          The one stop shop for all your hipster needs. Pedal up, get your pour,
+          and have one of our trusted mechanics look over your bike.
+        </Text>
+        <MapView
+          // TODO add a conditional to check for android or configure to use gMaps
+          style={styles.map}
+          //   TODO set to austin
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        />
+      </VStack>
+    </ScrollView>
   );
+}
+
+function getStyles() {
+  return {
+    container: {
+      alignItems: 'center',
+    },
+    map: {
+      height: 300,
+      width: 400,
+    },
+    text: {
+      marginTop: 50,
+      marginBottom: 50,
+      width: '70%',
+      textAlign: 'center',
+    },
+  };
 }
