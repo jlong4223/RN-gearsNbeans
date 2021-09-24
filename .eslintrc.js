3febcb989bd69702ed05bc833b2361eaf592c100
Jared Long <jlong4223@gmail.com>
2021-09-24 14:05:52 -0500

    feat: updates eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community'],
   rules: {
     'react/prop-types': 1,
     'no-console': 1,
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
   },
 };
