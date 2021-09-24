e9bb24e833dc0878596fab06923c96d2c4f030e2
Jared Long <jlong4223@gmail.com>
2021-09-23 19:03:32 -0500

    feat: installs module-resolver and uses aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
+const pkg = require('./package.json');
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: pkg._moduleAliases,
+      },
+    ],
+  ],
 };
