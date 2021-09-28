6a764f5a562f3779317172d06cea25ac9df4f9fb
Jared Long <jlong4223@gmail.com>
2021-09-27 22:05:18 -0500

    feat: adds get req for products

--- src/services/gb-reviews.js
+++ src/services/gb-reviews.js
@@ -4,3 +4,8 @@ export const getReviews = async () => {
   const reviews = await gbApi.get('/reviews');
   return reviews;
 };
+
+export const getProducts = async () => {
+  const products = await gbApi.get('/products');
+  return products;
+};
