43554fb05e219f3b70fdb626a5523e80d77c08e9
Jared Long <jlong4223@gmail.com>
2021-10-06 22:02:31 -0500

    feat: adds styles to cart msg

--- src/components/screens/Cart/CartMessage.js
+++ src/components/screens/Cart/CartMessage.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text } from 'native-base';
+import { Text, VStack } from 'native-base';
 
-export default function CartMessage({ itemCount, total }) {
+export default function CartMessage({ itemCount }) {
+  const styles = getStyles();
+
   return !itemCount ? (
-    <Text>No items in cart.</Text>
+    <Text style={styles.noItemContainer}>No items in cart.</Text>
   ) : (
-    <>
+    <VStack style={styles.container}>
       <Text>You have {itemCount} item(s) in your cart</Text>
-      <Text>Total: ${total}</Text>
-    </>
+    </VStack>
   );
+}
+
+function getStyles() {
+  return {
+    container: {
+      alignItems: 'center',
+      marginTop: 10,
+    },
+    noItemContainer: {
+      textAlign: 'center',
+      marginTop: 10,
+    },
+  };
 }
 
 CartMessage.propTypes = {
   itemCount: PropTypes.number,
-  total: PropTypes.number,
 };
