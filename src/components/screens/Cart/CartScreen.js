ed1fa20c28b6a8fbbf48da8c9f1ebf9a3b8c3ecc
Jared Long <jlong4223@gmail.com>
2021-10-06 22:05:08 -0500

    feat: setup cart screen w/redux & w/cart items

--- src/components/screens/Cart/CartScreen.js
+++ src/components/screens/Cart/CartScreen.js
@@ -1,20 +1,96 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { View } from 'native-base';
+import { removeFromCart, clearEntireCart } from '~actions/cartActions';
+import {
+  ScrollView,
+  Text,
+  View,
+  HStack,
+  VStack,
+  Pressable,
+  Button,
+} from 'native-base';
 import CartMessage from '~screens/Cart/CartMessage';
-function CartScreen({ cartItems }) {
+import CheckoutBtn from '~screens/Cart/CheckoutBtn';
+
+function CartScreen({ cartItems, removeFromCart, clearEntireCart }) {
   const { total, cart, itemCount } = cartItems;
+  const styles = getStyles();
+  console.log('cart: ', cart);
+
+  // TODO move to own component
+  const renderCartItems = () => {
+    return cart.map(item => (
+      <HStack key={item._id} style={styles.container}>
+        <Pressable onPress={() => console.log('pressed item')}>
+          <HStack style={styles.cartCard}>
+            <VStack>
+              <Text fontSize="lg">{item.name}</Text>
+              <Text style={styles.description}>{item.description}</Text>
+            </VStack>
+            <VStack>
+              <Text>${item.price}</Text>
+              <Text>Quantity: {item.quantity}</Text>
+              <Button onPress={() => removeFromCart(item)}>Remove</Button>
+            </VStack>
+          </HStack>
+        </Pressable>
+      </HStack>
+    ));
+  };
 
   return (
-    <View>
+    <>
       <CartMessage total={total} itemCount={itemCount} />
-    </View>
+      <ScrollView>{renderCartItems()}</ScrollView>
+      {itemCount > 0 && (
+        <>
+          <View style={styles.btnContainer}>
+            <Button onPress={clearEntireCart} style={styles.clearBtn}>
+              <Text>Clear Entire Cart</Text>
+            </Button>
+          </View>
+          <CheckoutBtn total={total} />
+        </>
+      )}
+    </>
   );
+}
+
+function getStyles() {
+  return {
+    container: {
+      justifyContent: 'space-around',
+    },
+    cartCard: {
+      marginTop: 15,
+      height: 90,
+      padding: 5,
+      justifyContent: 'space-between',
+      width: '90%',
+    },
+    description: {
+      width: 250,
+      marginLeft: 10,
+    },
+    btnContainer: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    clearBtn: {
+      height: 50,
+      width: '90%',
+      marginBottom: 10,
+      borderRadius: 20,
+    },
+  };
 }
 
 CartScreen.propTypes = {
   cartItems: PropTypes.object,
+  removeFromCart: PropTypes.func,
+  clearEntireCart: PropTypes.func,
 };
 
 const mapStateToProps = state => {
@@ -23,4 +99,6 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(CartScreen);
+export default connect(mapStateToProps, { removeFromCart, clearEntireCart })(
+  CartScreen,
+);
