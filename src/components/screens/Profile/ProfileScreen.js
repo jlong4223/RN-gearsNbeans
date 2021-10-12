2977882ec935170691276575d66bdfa8cc32ab09
Jared Long <jlong4223@gmail.com>
2021-10-11 22:21:01 -0500

    feat: moved logout & added ternary for profile pg

--- src/components/screens/Profile/ProfileScreen.js
+++ src/components/screens/Profile/ProfileScreen.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, VStack } from 'native-base';
+import { Button, VStack, Text } from 'native-base';
+import * as authActions from '~actions/authActions';
 import { goToSignIn } from '~app/navigation';
 import PropTypes from 'prop-types';
-function ProfileScreen({ userInfo }) {
+function ProfileScreen({ userInfo, logoutUser }) {
   console.log(userInfo);
   const styles = getStyles();
 
   return (
     <VStack style={styles.container}>
-      <Button onPress={goToSignIn} style={styles.btn}>
-        Sign In
-      </Button>
-      {/* TODO add screen for registering */}
-      <Button style={styles.btn}>Create an Account </Button>
+      {userInfo.user.user ? (
+        <>
+          <Text>Hi, {userInfo.user.user.name}</Text>
+          <VStack>
+            <Text>Here is your profile info:</Text>
+            <Text>{userInfo.user.user.email}</Text>
+            <Button onPress={logoutUser}>Logout</Button>
+          </VStack>
+        </>
+      ) : (
+        <>
+          <Button onPress={goToSignIn} style={styles.btn}>
+            Sign In
+          </Button>
+          {/* TODO add screen for registering */}
+          <Button style={styles.btn}>Create an Account </Button>
+        </>
+      )}
     </VStack>
   );
 }
@@ -34,10 +48,11 @@ function getStyles() {
 
 ProfileScreen.propTypes = {
   userInfo: PropTypes.object,
+  logoutUser: PropTypes.func,
 };
 
 const mapStateToProps = state => ({
   userInfo: state.userData,
 });
 
-export default connect(mapStateToProps)(ProfileScreen);
+export default connect(mapStateToProps, authActions)(ProfileScreen);
---
+++ src/components/screens/Profile/SignInScreen.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { Input, View, Button } from 'native-base';
+import * as authActions from '~actions/authActions';
+import PropTypes from 'prop-types';
+import { loginFields } from '~sharedComponents/AuthForms/fields';
+
+function SignInScreen({ loginUser }) {
+  const styles = getStyles();
+
+  const [userFieldValues, setUserFieldValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (name, value) => {
+    setUserFieldValues({ ...userFieldValues, [name]: value });
+  };
+
+  function handleSubmit() {
+    loginUser(userFieldValues);
+    setUserFieldValues({
+      email: '',
+      password: '',
+    });
+  }
+
+  function setLoginFields() {
+    return loginFields.map(field => (
+      <Input
+        style={styles.btnAndInput}
+        key={field.name}
+        type={field.type}
+        _focus={styles.inputFocus}
+        isRequired={field.required}
+        placeholder={field.placeholder}
+        onChangeText={value => handleChange(field.name, value)}
+      />
+    ));
+  }
+
+  return (
+    <View style={styles.container}>
+      {setLoginFields()}
+      <Button style={styles.btnAndInput} onPress={handleSubmit}>
+        Sign in
+      </Button>
+    </View>
+  );
+}
+
+function getStyles() {
+  return {
+    container: {
+      flex: 0.3,
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+    btnAndInput: {
+      //   margin: 10,
+      width: '60%',
+    },
+    inputFocus: {
+      width: '60%',
+    },
+  };
+}
+
+SignInScreen.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+};
+
+export default connect(null, authActions)(SignInScreen);
