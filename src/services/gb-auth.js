cb866a465e9ff687862009ac6479cc076daaefd9
Jared Long <jlong4223@gmail.com>
2021-10-22 07:40:10 -0500

    feat: sets up registration auth service

--- src/services/gb-auth.js
+++ src/services/gb-auth.js
@@ -3,3 +3,15 @@ import gbApi from '~services/gb-api';
 export const login = userDetails => {
   return gbApi.post('/login', { ...userDetails });
 };
+
+export const register = userInput => {
+  const { email, password, firstName, lastName } = userInput;
+
+  const userDetails = {
+    name: `${firstName} ${lastName}`,
+    email,
+    password,
+  };
+
+  return gbApi.post('/signup', { ...userDetails });
+};
