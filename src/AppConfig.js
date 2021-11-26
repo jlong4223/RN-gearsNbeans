46fba8c026e06bcb398854c2c50a2ac5bb02abf9
Jared Long <jlong4223@gmail.com>
2021-11-26 15:16:02 -0600

    fix: sets up specific err to ignore

--- src/AppConfig.js
+++ src/AppConfig.js
@@ -1,7 +1,8 @@
 import { LogBox } from 'react-native';
 import { configApp } from '~components/app/screenRegister';
 
-// this removes warnings on simulators but they still show in the console
-LogBox.ignoreAllLogs();
+LogBox.ignoreLogs([
+  'When server rendering, you must wrap your application in an <SSRProvider> to ensure consistent ids are generated between the client and server.',
+]);
 
 configApp();
