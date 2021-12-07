2d110afccc467b7ef90213e06781414df95b8856
Jared Long <jlong4223@gmail.com>
2021-12-07 17:11:48 -0600

    exporting actions from all actions

--- src/redux/actions/allActions.js
+++ src/redux/actions/allActions.js
@@ -1,15 +1,32 @@
 // TODO import all actions here and export them
 import {
-  getCartItems,
   addQuantityToCartItem,
   addToCart,
+  removeFromCart,
+  clearEntireCart,
+  removeQuantityFromCartItem,
 } from '~actions/cartActions';
-import { getGBBikeServices, getGBProducts } from '~actions/reviewsActions';
+import {
+  getGBBikeServices,
+  getGBProducts,
+  getGBReviews,
+  createReview,
+  deleteUserReview,
+  updateReviewObject,
+  filterReviews,
+} from '~actions/reviewsActions';
 
 export {
-  getCartItems,
   getGBBikeServices,
   addQuantityToCartItem,
   addToCart,
   getGBProducts,
+  removeFromCart,
+  clearEntireCart,
+  removeQuantityFromCartItem,
+  getGBReviews,
+  createReview,
+  deleteUserReview,
+  updateReviewObject,
+  filterReviews,
 };
