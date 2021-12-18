d72dd49e51c403e99fbca4fab4a8731187055e0b
Jared Long <jlong4223@gmail.com>
2021-12-18 16:56:55 -0600

    feat: adds react native config

--- src/services/gb-api.js
+++ src/services/gb-api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
+import Config from 'react-native-config';
 
 export default axios.create({
-  baseURL: 'https://gearsbeans-api.herokuapp.com',
+  baseURL: Config.BASEURL,
 });
