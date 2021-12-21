958f810b1afc103581a91cc5787fb57700b4b1bd
Jared Long <jlong4223@gmail.com>
2021-12-21 16:49:23 -0600

    feat: turns login into async/await

--- src/services/gb-auth.js
+++ src/services/gb-auth.js
@@ -1,7 +1,7 @@
 import gbApi from '~services/gb-api';
 
-export const login = userDetails => {
-  return gbApi.post('/login', { ...userDetails });
+export const login = async userDetails => {
+  return await gbApi.post('/login', { ...userDetails });
 };
 
 export const register = userInput => {
